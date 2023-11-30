f3b4fa7a76954cc085de46eb0346a5c93c4b8ce3
TEX_479 <109556530+TEX479@users.noreply.github.com>
2023-11-30 23:55:17 +0100

    Add files via upload
    added comments

--- code_003.py
+++ code_003.py
@@ -19,19 +19,25 @@ numOfElves = [0] * double_new_lines
 
 #setup for re-looping to get the elves calorie-numbers
 elve_index = 0
-number_total = 0
-number_for_calculation = 0
+caloriesTotal = 0
+caloriesInProgress = 0
+#loop to fill the "numOfElves" array
 for i in range(length-1):
+    #if there is a new elve incoming, add the calorieTotal to the current numOfElves[i]
     if ord(buffer[i]) == 10 and ord(buffer[i+1]) == 10:
-        numOfElves[elve_index] = number_total + number_for_calculation
+        numOfElves[elve_index] = caloriesTotal + caloriesInProgress
         elve_index += 1
-        number_total = 0
-        number_for_calculation = 0
+        caloriesTotal = 0
+        caloriesInProgress = 0
+    #if there is not a new line, add the char to the caloriesInProgress (to get the int of a multi-charred value)
+    #this effectively adds the single chars together to the decimal number
     if ord(buffer[i]) != 10:
-        number_for_calculation = number_for_calculation * 10
-        number_for_calculation = number_for_calculation + ord(buffer[i]) - 48
+        caloriesInProgress = caloriesInProgress * 10
+        caloriesInProgress = caloriesInProgress + ord(buffer[i]) - 48
+    #if there is a new line, add the calculated decimal to the current elves total
     if ord(buffer[i]) == 10:
-        number_total += number_for_calculation
-        number_for_calculation = 0
+        caloriesTotal += caloriesInProgress
+        caloriesInProgress = 0
+
 print(max(numOfElves))
 print(numOfElves)
